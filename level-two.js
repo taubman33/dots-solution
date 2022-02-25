663b9086a750c059eb1de45c600115583c99eb1a
jataubman <jeremy.taubman@generalassemb.ly>
2022-02-25 16:26:11 -0500

    adding solution

--- level-two.js
+++ level-two.js
@@ -2,3 +2,21 @@
  * DOTS: Level Two
  *
  */
+let score = 0
+let arenaEl = document.querySelector('.js-arena')
+let scoreEl = document.querySelector('.js-score')
+
+arenaEl.addEventListener('click', (e) => {
+  if ( e.target.classList.contains('js-ball') ) {
+    score += 10
+    if ( score < 100 ) {
+      scoreEl.innerText = score
+    } else {
+      declareWinner()
+    }
+  }
+})
+
+function declareWinner() {
+  document.body.classList.add('game-over')
+}
